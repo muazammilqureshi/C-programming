f28ff5a0c91d66b7bde117f73e253c5a21e83a85
Muzammil Kureshi <muazammilqureshi@gmail.com>
2026-08-10 18:03:33 +0530

    Create 01-Basics/mul-two-numbers.c
    Add multiplication program

---
+++ 01-Basics/mul-two-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter a :");
+    scanf("%d", &a);
+
+    printf("Enter b :");
+    scanf("%d", &b);
+
+    printf("mul : %d", a * b);
+
+    return 0;
+}
