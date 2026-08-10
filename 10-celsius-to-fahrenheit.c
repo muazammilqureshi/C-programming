7d9d05b82e42cbcc588602a0f965eae8fc3627fb
Muzammil Kureshi <muazammilqureshi@gmail.com>
2026-08-10 13:05:24 +0530

    Create 10-celsius-to-fahrenheit.c
    Add Celsius to Fahrenheit program

---
+++ 10-celsius-to-fahrenheit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float c, f;
+
+    printf("Enter celsius :");
+    scanf("%f", &c);
+
+    f = (c * 9 / 5) + 32;
+
+    printf("fahrenheit : %.2f", f);
+
+    return 0;
+}
