bc329d62461e6a8eab6ad5e039e752841d7cb213
Muzammil Kureshi <muazammilqureshi@gmail.com>
2026-08-10 12:46:00 +0530

    Create 02-name-age-city.c
    Add name age city program

---
+++ 02-name-age-city.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  char name[10];
+     int age;
+   char city[10];
+
+     printf("Enter your name :");
+     scanf("%s",name);
+    printf("Enter your age :");
+     scanf("%d",&age);
+    printf("Enter your city :");
+     scanf("%s",city);
+     
+     
+     printf("\n name = %s",name);
+     printf("\n age = %d",age);
+     printf("\n city = %s",city);
+ 
+    return 0;
+}
+   
+     
