185d7bb85122c026e6439c27acda0a6a6262cab0
Muzammil Kureshi <muazammilqureshi@gmail.com>
2026-08-10 12:58:20 +0530

    Create 06-divide-two-numbers.c
    Add division program

---
+++ 06-divide-two-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b;
+
+    printf("Enter a :");
+    scanf("%f", &a);
+
+    printf("Enter b :");
+    scanf("%f", &b);
+
+    printf("div : %.2f", a / b);
+
+    return 0;
+}
